e6812f00ccc74777894554522b32f9058c0938c1
jenniferbusca <jennifer.busca@gmail.com>
2019-10-19 11:37:39 -0400

    Updates

---
+++ precache-manifest.7fed051ec3f1de93a82681b34207aade.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89dd17082456f41b7b06ba04fb087bf9",
+    "url": "/pixelate-app-client/index.html"
+  },
+  {
+    "revision": "f6e69d1437aab3c930cb",
+    "url": "/pixelate-app-client/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "21acff7b1a367dbecf08",
+    "url": "/pixelate-app-client/static/css/main.7b473faa.chunk.css"
+  },
+  {
+    "revision": "f6e69d1437aab3c930cb",
+    "url": "/pixelate-app-client/static/js/2.4c6f585c.chunk.js"
+  },
+  {
+    "revision": "21acff7b1a367dbecf08",
+    "url": "/pixelate-app-client/static/js/main.b55e60a9.chunk.js"
+  },
+  {
+    "revision": "a92b9344a76d89671d3e",
+    "url": "/pixelate-app-client/static/js/runtime-main.0ebd8920.js"
+  },
+  {
+    "revision": "99b024815ce217ba2128794ef382f074",
+    "url": "/pixelate-app-client/static/media/pixel_logo.99b02481.png"
+  }
+]);
