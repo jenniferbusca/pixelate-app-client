0b23bd60c0385bcd83e7ced06e303c2fd0d8c864
jenniferbusca <jennifer.busca@gmail.com>
2019-10-20 16:31:31 -0400

    Updates

--- precache-manifest.5b5144773ec2e9002818802bf9f0125d.js
+++ precache-manifest.5b5144773ec2e9002818802bf9f0125d.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c73fc83dc6f097e74c33a9fdfad33b29",
+    "revision": "fc71a17fc2608b750ac9c09822ec09e4",
     "url": "/pixelate-app-client/index.html"
   },
   {
@@ -8,23 +8,23 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/pixelate-app-client/static/css/2.0624914a.chunk.css"
   },
   {
-    "revision": "95275671f3e8e8f6bd1d",
-    "url": "/pixelate-app-client/static/css/main.6cd257ce.chunk.css"
+    "revision": "4fdc193dccd8805678b1",
+    "url": "/pixelate-app-client/static/css/main.5ecc7da5.chunk.css"
   },
   {
     "revision": "ec2db0f26ab1f0c5e71e",
     "url": "/pixelate-app-client/static/js/2.e91c0e59.chunk.js"
   },
   {
-    "revision": "95275671f3e8e8f6bd1d",
-    "url": "/pixelate-app-client/static/js/main.61facf8a.chunk.js"
+    "revision": "4fdc193dccd8805678b1",
+    "url": "/pixelate-app-client/static/js/main.9236e5e8.chunk.js"
   },
   {
     "revision": "a92b9344a76d89671d3e",
     "url": "/pixelate-app-client/static/js/runtime-main.0ebd8920.js"
   },
   {
-    "revision": "99b024815ce217ba2128794ef382f074",
-    "url": "/pixelate-app-client/static/media/pixel_logo.99b02481.png"
+    "revision": "edb7f28c484e91a1d356c49d8a0dddfe",
+    "url": "/pixelate-app-client/static/media/pixel_logo.edb7f28c.png"
   }
 ]);
